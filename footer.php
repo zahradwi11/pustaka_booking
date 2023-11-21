d5108f9fc7d698f853b8ce1e6858258baecc8fba
zahradwi11 <151638570+zahradwi11@users.noreply.github.com>
2023-11-21 20:30:35 +0700

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,27 @@
+<!-- Bootstrap core JavaScript-->
+<script src="<?= base_url('assets/'); 
+?>vendor/jquery/jquery.min.js"></script>
+<script src="<?= base_url('assets/'); 
+?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<!-- Core plugin JavaScript-->
+<script src="<?= base_url('assets/'); ?>vendor/jquery-easing/jquery.easing.min.js"></script>
+
+<!-- Custom scripts for all pages-->
+<script src="<?= base_url('assets/'); ?>js/sb-admin-2.min.js"></script>
+
+<script>
+    $('.custom-file-input').on('change', function() {
+        let fileName = $(this).val().split('\\').pop();
+         $(this).next('.custom-file-label').addClass("selected").html(fileName);
+    });
+
+    $(document).ready(function() {
+        $("#table-datatable").dataTable();
+    });
+    $('.alert-message').alert().delay(3500).slideUp('slow');
+</script>
+
+</body>
+
+</html>
